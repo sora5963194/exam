704f220de8ff945087109203f2ceff875e773fda
djangouser <user@wxample.com>
2026-05-18 10:18:10 +0900

    05/18　森　拓真

---
+++ src/main/java/bean/Test.java
@@ -0,0 +1,89 @@
+//ファイル名: Test.java
+
+package bean;
+
+import java.io.Serializable;
+
+import javax.security.auth.Subject;
+
+public class Test implements Serializable{
+	
+	/**
+	 * 学生番号
+	 */
+	private Student student ;
+
+	/**
+	 * 科目コード
+	 */
+	private Subject subject ;
+	
+	/**
+	 * 学校コード
+	 */
+	private School school ;
+	
+	/**
+	 * 回数
+	 */
+	private int no ;
+	
+	/**
+	 * 得点
+	 */
+	private int point ;
+	
+	/**
+	 * クラス番号
+	 */
+	private String classNum ;
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public Subject getSubject() {
+		return subject;
+	}
+
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+
+	public School getSchool() {
+		return school;
+	}
+
+	public void setSchool(School school) {
+		this.school = school;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public String getClassNum() {
+		return classNum;
+	}
+
+	public void setClassNum(String classNum) {
+		this.classNum = classNum;
+	}
+	
+}
---
+++ src/main/java/dao/TestDao.java
@@ -0,0 +1,62 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import bean.School;
+import bean.Student;
+import bean.Subject; // javax.security.auth.Subjectから変更
+import bean.Test;
+
+public class TestDao extends Dao {
+
+    /**
+     * getメソッド: 学生、科目、学校、回数を指定して成績情報を1件取得する
+     */
+    public Test get(Student student, Subject subject, School school, int no) throws Exception {
+        // テストインスタンスを初期化
+        Test test = null;
+        // データベースへのコネクションを確立
+        Connection connection = getConnection();
+        // プリペアードステートメント
+        PreparedStatement statement = null;
+
+        try {
+            // SQL文の作成（スクリーンショット 2026-05-14 121831.png の物理名称を参照）
+            String sql = "SELECT * FROM TEST WHERE STUDENT_NO = ? AND SUBJECT_CD = ? AND SCHOOL_CD = ? AND NO = ?";
+            
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, student.getNo());
+            statement.setString(2, subject.getCd());
+            statement.setString(3, school.getCd());
+            statement.setInt(4, no);
+
+            // クエリの実行
+            ResultSet rSet = statement.executeQuery();
+
+            if (rSet.next()) {
+                test = new Test();
+                // 取得した結果をBeanにセット
+                test.setStudent(student);
+                test.setSubject(subject);
+                test.setSchool(school);
+                test.setNo(rSet.getInt("NO"));
+                test.setPoint(rSet.getInt("POINT"));
+                test.setClassNum(rSet.getString("CLASS_NUM"));
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            // ステートメントとコネクションのクローズ
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+
+        return test;
+    }
+}
