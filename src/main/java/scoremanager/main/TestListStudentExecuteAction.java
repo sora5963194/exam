b1b12a673ab4cf8f43cc66039050b580c8193bcf
sora5963194 <knz2574583@stu.o-hara.ac.jp>
2026-05-25 09:58:11 +0900

    5月25日山本

--- src/main/java/scoremanager/main/TestListStudentExecuteAction.java
+++ src/main/java/scoremanager/main/TestListStudentExecuteAction.java
@@ -1,13 +1,20 @@
 package scoremanager.main;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import bean.Student;
+import bean.Subject;
 import bean.Teacher;
 import bean.TestListStudent;
+import dao.ClassNumDao;
 import dao.StudentDao;
+import dao.StudentlistDao;
+import dao.SubjectDao;
 import dao.TestListStudentDao;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -25,17 +32,50 @@ public class TestListStudentExecuteAction extends Action{
 		String studentNo = ""; // 入力された学籍番号
 		
         Student student = new Student();
+        
         StudentDao studentDao = new StudentDao();
-		TestListStudentDao dao = new TestListStudentDao();
+        SubjectDao subjectDao = new SubjectDao();
+		ClassNumDao classNumDao = new ClassNumDao();
+        TestListStudentDao dao = new TestListStudentDao();
+        StudentlistDao studentlistDao = new StudentlistDao();
 		Map<String, String> errors = new HashMap<>(); // エラーメッセージ
 		
 		//リクエストパラメーターの取得2
 		studentNo = req.getParameter("f4");
 		
-		//学生情報を取得
-		student = studentDao.get(studentNo);
+		// クラス一覧
+        List<String> class_list = classNumDao.filter(teacher.getSchool());
+        Collections.sort(class_list);
+        
+        // 科目一覧
+        List<Subject> subject_list = subjectDao.filter(teacher.getSchool().getCd());
+        subject_list.sort(Comparator.comparing(Subject::getCd));
+        
+        // 入学年度一覧
+        List<Student> studentList = studentlistDao.filter();
+        List<Integer> entYearList = new ArrayList<>();
+        
+        for (Student studentl : studentList) {
+
+        	// 入学年度取得
+        	int year = studentl.getEntYear();
+
+        	// 重複していなければ追加
+        	if (!entYearList.contains(year)) {
+                entYearList.add(year);
+            }
+        }
+        // 入学年度を昇順ソート
+     	Collections.sort(entYearList);
+     	
+     	// JSPへセット
+        req.setAttribute("cNumlist", class_list);
+        req.setAttribute("entYearSet", entYearList);
+        req.setAttribute("list", subject_list);
 		
-		//ビジネスロジック4
+        //学生情報を取得
+      	student = studentDao.get(studentNo);
+        //ビジネスロジック4
 		if (student == null) {
 
 		    errors.put("1", "学生が存在しません");
@@ -59,6 +99,7 @@ public class TestListStudentExecuteAction extends Action{
 	
 	            // 成績一覧
 	            req.setAttribute("tlslist", testList);
+	            
 	        }
 			}
 
